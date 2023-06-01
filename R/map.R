47b4588321efcf2f6b8bba9b1ffccc540e7378be
mmollina <mmollina@gmail.com>
2023-05-31 21:37:41 -0400

    Implementing single parent HMM

--- R/map.R
+++ R/map.R
@@ -10,32 +10,55 @@ hmm_map_reconstruction <- function(x,
                                    verbose = FALSE,
                                    tol = 10e-4)
 {
+  assert_that(is.mappoly2.sequence(x))
   assert_that(all(length(x$mrk.names) == sapply(p, nrow)))
   assert_that(x$data$ploidy.p1 == ncol(p[[1]]))
   assert_that(x$data$ploidy.p2 == ncol(p[[2]]))
   assert_that(all(apply(p[[1]], 1, sum) == x$data$dosage.p1[x$mrk.names]))
   assert_that(all(apply(p[[2]], 1, sum) == x$data$dosage.p2[x$mrk.names]))
   g <- x$data$geno.dose[x$mrk.names, ]
-  pedigree <- matrix(rep(c(1,
-                           2,
-                           x$data$ploidy.p1,
-                           x$data$ploidy.p2, 1),
-                         x$data$n.ind),
-                     nrow = x$data$n.ind,
-                     byrow = TRUE)
   ret_H0 <- TRUE
   if(is.null(rf)){
     rf <- rep(0.01, nrow(g) - 1)
     ret_H0 <- FALSE
   }
   assert_that(length(rf) == nrow(g) - 1)
-  w <- est_hmm_map_biallelic(PH = p,
+  if(detect_info_par(x) == "p1"){
+    id <- which(x$data$ploidy.p2 == x$data$dosage.p2)
+    g[id, ] <- g[id, ] - x$data$ploidy.p2/2
+    w <- mappoly2:::est_hmm_map_biallelic_single(PH = p[[1]],
+                                                 G = g,
+                                                 rf = rf,
+                                                 verbose = verbose,
+                                                 tol = tol,
+                                                 ret_H0 = ret_H0)
+  } else if (detect_info_par(x) == "p2"){
+    id <- which(x$data$ploidy.p1 == x$data$dosage.p1)
+    g[id, ] <- g[id, ] - x$data$ploidy.p1/2
+    w <- mappoly2:::est_hmm_map_biallelic_single(PH = p[[2]],
+                                                 G = g,
+                                                 rf = rf,
+                                                 verbose = verbose,
+                                                 tol = tol,
+                                                 ret_H0 = ret_H0)
+  } else if (detect_info_par(x) == "both"){
+    pedigree <- matrix(rep(c(1,
+                             2,
+                             x$data$ploidy.p1,
+                             x$data$ploidy.p2, 1),
+                           x$data$n.ind),
+                       nrow = x$data$n.ind,
+                       byrow = TRUE)
+    w <- est_hmm_map_biallelic(PH = p,
                                G = g,
                                pedigree = pedigree,
                                rf = rf,
                                verbose = verbose,
                                tol = tol,
                                ret_H0 = ret_H0)
+  } else {
+    stop("it should not get here")
+  }
   names(w) <- c("loglike", "rec.frac")
   return(w)
 }
--- R/utils.R
+++ R/utils.R
@@ -4,3 +4,31 @@ get_seq_indices <- function(input.seq)
 embedded_to_numeric <- function(x) {
   as.integer(gsub("[^0-9]", "", x))
 }
+
+#' Detects which parent is informative
+#'
+#' @param x an object of class \code{mappoly2.sequence}
+#'
+#' @export
+detect_info_par<-function(x){
+  ## checking for correct object
+  if (is.mappoly2.sequence(x)) {
+    if(all(x$data$dosage.p2 == 0 | x$data$dosage.p2 == x$data$ploidy.p2))
+      return("p1")
+    if(all(x$data$dosage.p1 == 0 | x$data$dosage.p1 == x$data$ploidy.p1))
+      return("p2")
+    else
+      return("both")
+  }
+  # else if (inherits(x, "mappoly.map")) {
+  #   if(all(x$info$seq.dose.p2 == 0 | x$info$seq.dose.p2 == x$info$ploidy))
+  #     return("p1")
+  #   else if(all(x$info$seq.dose.p1 == 0 | x$info$seq.dose.p1 == x$info$ploidy))
+  #     return("p2")
+  #   else
+  #     return("both")
+  # }
+  else{
+    stop(deparse(substitute(x)), " is not an object of class 'mappoly2.sequence'")
+  }
+}
