4b5870f32d553955d426b381516d908e2244d41d
mmollina <mmollina@gmail.com>
2023-06-14 18:31:06 -0400

    update tests

--- test_IV.R
+++ test_IV.R
@@ -60,7 +60,7 @@ for(i in seq(30,0,-5)){
 image(x)
 
 ########## Testing vs_inserted_mrk #########
-i<-5
+i<-10
 m <- rf_list_to_matrix(tpt,
                        thresh.LOD.ph = i,
                        shared.alleles = TRUE)
@@ -72,16 +72,17 @@ dose.vec <- s1$data$dosage.p1[mrk.id]
 S <- m$Sh.p1[mrk.id, mrk.pos]
 verbose <- TRUE
 L1 <- mappoly2:::phasing_one(mrk.id, dose.vec, S, InitPh, verbose)
-L1[[48]]
+L1[[137]]
 ##P2
 InitPh <- s1$phases[[1]]$p2
 dose.vec <- s1$data$dosage.p2[mrk.id]
 S <- m$Sh.p2[mrk.id, mrk.pos]
 verbose <- TRUE
 L2 <- mappoly2:::phasing_one(mrk.id, dose.vec, S, InitPh, verbose)
-L2[[48]]
-L1[[48]]
-G <- s1$data$geno.dose[mrk.id[48], ,drop = TRUE]
+L2[[137]]
+L1[[137]]
+G <- s1$data$geno.dose[mrk.id[137], ,drop = TRUE]
+G[is.na(G)] <- -1
 pedigree <- matrix(rep(c(1,
                          2,
                          s1$data$ploidy.p1,
@@ -90,33 +91,30 @@ pedigree <- matrix(rep(c(1,
                    nrow = s1$data$n.ind,
                    byrow = TRUE)
 s1<-calc_haplotypes(s1)
-mrk.id[48]
-s1$phases[[1]]$p1[18:19,]
-s1$phases[[1]]$p2[18:19,]
-homolog_prob <- as.matrix(s1$phases[[1]]$haploprob[,18:19+2])
+mrk.id[137]
+s1$phases[[1]]$p1[c("Ch_1_M_249", "Ch_1_M_252"),]
+s1$phases[[1]]$p2[c("Ch_1_M_249", "Ch_1_M_252"),]
+match(c("Ch_1_M_249", "Ch_1_M_252"), rownames(s1$phases[[1]]$p1))
+homolog_prob <- as.matrix(s1$phases[[1]]$haploprob[,113:114+2])
 
-L2[[48]]
-L1[[48]]
-PH1 <- list(L1[[48]][1,,drop=TRUE], L2[[48]][1,,drop = TRUE])
-z1<-mappoly2:::est_hmm_map_biallelic_insert_marker(PH1,
-                                                   G,
-                                                   pedigree,
-                                                   homolog_prob,
-                                                  rf = c(0.01,0.01),
-                                                  verbose = TRUE,
-                                                  detailed_verbose = FALSE,
-                                                  tol = 10e-4,
-                                                  ret_H0 = FALSE)
-PH2 <- list(L1[[48]][1,,drop=TRUE], L2[[48]][2,,drop = TRUE])
-z2<-mappoly2:::est_hmm_map_biallelic_insert_marker(PH2,
-                                                   G,
-                                                   pedigree,
-                                                   homolog_prob,
-                                                   rf = c(0.01,0.01),
-                                                   verbose = TRUE,
-                                                   detailed_verbose = FALSE,
-                                                   tol = 10e-4,
-                                                   ret_H0 = FALSE)
-z1
-z2
 
+mrk.pos <- 137
+
+z<-vector("list", nrow(L1[[mrk.pos]]) * nrow(L2[[mrk.pos]]))
+
+for(j in 1:nrow(L1[[mrk.pos]])){
+  for(i in 1:nrow(L2[[mrk.pos]])){
+    PH <- list(L1[[mrk.pos]][j,,drop=TRUE], L2[[mrk.pos]][i,,drop = TRUE])
+    z[[i]]<-mappoly2:::est_hmm_map_biallelic_insert_marker(PH,
+                                                           G,
+                                                           pedigree,
+                                                           homolog_prob,
+                                                           rf = c(0.01,0.01),
+                                                           verbose = TRUE,
+                                                           detailed_verbose = FALSE,
+                                                           tol = 10e-4,
+                                                           ret_H0 = FALSE)
+  }
+}
+which.max(sapply(z, function(x) x[[1]]))
+t(sapply(z, function(x) x[[2]]))
